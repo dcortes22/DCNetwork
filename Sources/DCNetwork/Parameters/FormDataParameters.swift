18f888d8836be60ff7319210cc5827b31a69f255
David Cortes <dcortes22@gmail.com>
2025-04-25 00:44:34 -0600

    Created new fix to flatten dictionary on formdata

--- Sources/DCNetwork/Parameters/FormDataParameters.swift
+++ Sources/DCNetwork/Parameters/FormDataParameters.swift
@@ -21,17 +21,17 @@ struct FormDataParameters: SerializableParameter {
     
     func serialize() throws -> Data {
         var body = Data()
-                
+        
         // Agregar parámetros de texto al form-data
-        for (key, value) in parameters {
+        let flatParams = flatten(dictionary: parameters)
+
+        for (key, value) in flatParams {
             body.append("--\(boundary)\r\n".data(using: .utf8)!)
             body.append("Content-Disposition: form-data; name=\"\(key)\"\r\n\r\n".data(using: .utf8)!)
-            if let valueData = Utils.convertToString(value)?.data(using: .utf8) {
-                body.append(valueData)
-            }
+            body.append(value.data(using: .utf8)!)
             body.append("\r\n".data(using: .utf8)!)
         }
-        
+
         for file in files {
             body.append("--\(boundary)\r\n".data(using: .utf8)!)
             body.append("Content-Disposition: form-data; name=\"\(file.name)\"; filename=\"\(file.fileName)\"\r\n".data(using: .utf8)!)
@@ -39,8 +39,33 @@ struct FormDataParameters: SerializableParameter {
             body.append(file.data)
             body.append("\r\n".data(using: .utf8)!)
         }
-        
+
         body.append("--\(boundary)--\r\n".data(using: .utf8)!)
         return body
+    }
+    
+    private func flatten(dictionary: [String: Any], prefix: String? = nil) -> [(String, String)] {
+        var result: [(String, String)] = []
+
+        for (key, value) in dictionary {
+            let fullKey = prefix != nil ? "\(prefix!)[\(key)]" : key
+
+            switch value {
+            case let nested as [String: Any]:
+                result.append(contentsOf: flatten(dictionary: nested, prefix: fullKey))
+            case let string as String:
+                result.append((fullKey, string))
+            case let int as Int:
+                result.append((fullKey, String(int)))
+            case let double as Double:
+                result.append((fullKey, String(double)))
+            case let bool as Bool:
+                result.append((fullKey, String(bool)))
+            default:
+                break // omit nil or unsupported types
+            }
+        }
+
+        return result
     }
 }
